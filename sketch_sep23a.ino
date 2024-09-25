504dc271c83148b76033c46b583c9effc731e5fe
tetaea <161100284+tetaea@users.noreply.github.com>
2024-09-25 16:24:55 +0900

    LEDcontrol (05P12)

--- sketch_sep23a.ino
+++ sketch_sep23a.ino
@@ -7,11 +7,11 @@ void setup() {
 
 void loop() {
   // 처음 1초 동안 LED 켜기
-  digitalWrite(PIN_LED, HIGH);  // LED 켜기
+  digitalWrite(PIN_LED, 0);  // LED 켜기
   delay(1000);                  // 1초 대기
 
   // 다음 1초 동안 LED 5회 깜빡이기
-  for (int i = 0; i < 5; i++) {
+  for (int i = 0; i < 6; i++) {
     digitalWrite(PIN_LED, LOW);   // LED 끄기
     delay(100);                   // 0.1초 대기
     digitalWrite(PIN_LED, HIGH);  // LED 켜기
@@ -19,7 +19,7 @@ void loop() {
   }
 
   // LED 끄고 무한 루프에 진입
-  digitalWrite(PIN_LED, LOW);   // LED 끄기
+  digitalWrite(PIN_LED, HIGH);   // LED 끄기
   while (1) {
     // 무한 루프
   }
